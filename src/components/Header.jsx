8c7527e5fb9f89ce82d6d678d025e51b0e765ec0
hng011 <hansnaufal.3040@gmail.com>
2024-01-11 20:55:55 +0700

    add animation and readme.md

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './../assets/css/App.css'
 
 const Header = () => {
   return (
     <header>
       <h1 className='text-5xl font-bold'>NumFacts</h1>
       <code><i>created by Hans-5021591</i></code>
+      <p className='text-lg desc-animation'>👾🙆 UNLOCK THE UNIVERSE'S SECRET 🙆👾</p>
     </header>
   );
 }
